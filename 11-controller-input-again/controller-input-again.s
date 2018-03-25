f3f74e2ef50ca20d178d54e304299104c3dc38bb
taylus <therealbjohnson@gmail.com>
2018-03-24 19:49:58 -0500

    Adding a little animated slime guy

--- 11-controller-input-again/controller-input-again.s
+++ 11-controller-input-again/controller-input-again.s
@@ -9,6 +9,9 @@
 buttons: .res 1             ; current frame's controller button states
 buttons_old: .res 1         ; last frame's controller button states
 released_buttons: .res 1    ; buttons that were released this frame (were pressed last frame but not the current frame, see get_released_buttons)
+frame_counter: .res 1       ; counts up every frame for timing events
+
+OAM_BASE_ADDR = $0200       ; location in work RAM where sprite data is stored (before DMA to VRAM)
 
 .segment "HEADER"
     .byte "NES", $1A        ; signature
@@ -20,18 +23,18 @@ reset:                      ; adapted from https://wiki.nesdev.com/w/index.php/I
     sei                     ; disable IRQs
     cld                     ; disable decimal mode
 clear_memory:
-  lda #$00
-  sta $0000, x
-  sta $0100, x
-  sta $0300, x
-  sta $0400, x
-  sta $0500, x
-  sta $0600, x
-  sta $0700, x
-  lda #$FE                  ; move all sprites off the screen
-  sta $0200, x
-  inx
-  bne clear_memory
+    lda #$00
+    sta $0000, x
+    sta $0100, x
+    sta $0300, x
+    sta $0400, x
+    sta $0500, x
+    sta $0600, x
+    sta $0700, x
+    lda #$FE                ; move all sprites off the screen
+    sta OAM_BASE_ADDR, x
+    inx
+    bne clear_memory
 vblank_wait_1:              ; first wait for vblank to make sure the PPU is ready
     lda PPU_STATUS
     bpl vblank_wait_1
@@ -50,17 +53,37 @@ load_palette_loop:          ; copy all 32 bytes of palette data into VRAM
     lda palette_data, x     ; relative load from label + X
     sta PPU_DATA
     inx
-    cpx #$10                ; all 16 bytes copied?
+    cpx #$20                ; all 32 bytes copied?
     bne load_palette_loop   ; if not, keep looping
+load_sprite_oam:
+    ldx #$00
+load_sprite_loop:
+    lda initial_sprite_data, x
+    sta $0200, x
+    inx
+    cpx #$24                ; all 36 bytes copied?
+    bne load_sprite_loop    ; if not, keep looping
 enable_ppu:
-    lda #%10000000
-    sta PPU_CTRL        ; enable NMI, background and sprites both use pattern table 0
+    lda #%10001000
+    sta PPU_CTRL            ; enable NMI, background uses pattern table 0, sprites use pattern table 1
     lda #%00011110
-    sta PPU_MASK        ; enable sprites, enable background, no clipping on left side
+    sta PPU_MASK            ; enable sprites, enable background, no clipping on left side
 loop:
-    jmp loop            ; loop forever
+    jmp loop                ; loop forever
     
 nmi:
+    inc frame_counter
+copy_sprite_oam:
+    lda #$00
+    sta OAM_ADDR                ; set the low byte ($00) of the RAM address
+    lda #$02
+    sta OAM_DMA                 ; set the high byte ($02) of the RAM address + start the transfer
+animate:
+    lda frame_counter
+    and #$7F                    ; mask out the most significant bit
+    bne read_input              ; branch if non-zero to blink every 128 frames
+    jsr blink
+read_input:
     lda buttons
     sta buttons_old             ; save copy of last frame's buttons
     jsr read_joypad1            ; read current frame's controller buttons
@@ -252,6 +275,35 @@ input_done:
     rts
 .endproc
 
+; update sprites to make roly blink --
+; toggle sprite 1 between tile #$04 (eyes closed) and #$01 (eyes open)
+.proc blink
+    lda $0205
+    cmp #$01            ; eyes open?
+    beq close_eyes      ; then close
+open_eyes:              ; else open
+    ;lda #$01
+    ;sta $0205
+    set_sprite_tile $1, $1
+    rts
+close_eyes:
+    ;lda #$04
+    ;sta $0205
+    set_sprite_tile $1, $4
+    rts
+.endproc
+
+; sprite attribute data to load on startup
+; y position, tile #, attributes, x position
+; for more info, see http://wiki.nesdev.com/w/index.php?title=PPU_OAM
+initial_sprite_data:
+    .byte $98, $00, $00, $68    ; sprite 0 (top left)
+    .byte $98, $01, $00, $70    ; sprite 1 (top middle)
+    .byte $98, $02, $00, $78    ; sprite 2 (top right)
+    .byte $A0, $10, $00, $68    ; sprite 3 (bottom left)
+    .byte $A0, $11, $00, $70    ; sprite 4 (bottom middle)
+    .byte $A0, $12, $00, $78    ; sprite 5 (bottom right)
+
 ; screen tile numbers and attribute table (palettes)
 nametable:
     .incbin "gfx\nesjoy.nam"
@@ -260,6 +312,15 @@ nametable:
 ; available colors: https://wiki.nesdev.com/w/index.php/PPU_palettes#2C02
 palette_data:
     .incbin "gfx\nesjoy.pal"
+    ; sprite colors to be loaded into $3F10
+    .byte $0F             ; universal background color
+    .byte $01, $30, $06   ; sprite palette 0 (blue, white, red)
+    .byte $0F             ; ignored
+    .byte $00, $00, $00   ; sprite palette 1 (unused)
+    .byte $0F             ; ignored
+    .byte $00, $00, $00   ; sprite palette 2 (unused)
+    .byte $0F             ; ignored
+    .byte $00, $00, $00   ; sprite palette 3 (unused)
 
 .segment "VECTORS"
     .word nmi, reset, 0
